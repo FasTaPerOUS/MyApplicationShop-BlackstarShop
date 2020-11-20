6bace1e8d580fd0311bd7dac326eeea3051c94bd
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2020-11-20 18:43:28 +0300

    added empty project

---
+++ MyShop_BlackStar/MyShop_BlackStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyShop_BlackStar
+//
+//  Created by Norik on 20.11.2020.
+//  Copyright © 2020 Norik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
