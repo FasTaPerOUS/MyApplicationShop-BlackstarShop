3a16f1560d0a25af9699e346be6a5846c37e1680
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2020-12-10 15:04:00 +0300

    added item files

---
+++ MyShop_BlackStar/MyShop_BlackStar/ItemViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ItemViewController.swift
+//  MyShop_BlackStar
+//
+//  Created by Norik on 08.12.2020.
+//  Copyright © 2020 Norik. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+    
+    var info = OneItemWithAllColors(name: "", description: "", colorName: [], sortOrder: 0, mainImage: [], productImages: [], offers: [], price: [])
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
