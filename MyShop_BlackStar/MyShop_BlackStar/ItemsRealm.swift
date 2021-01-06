101de1b53fe911e5514a26572d1dc253f618cb19
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2021-01-06 17:05:32 +0300

    Realm of Items class

---
+++ MyShop_BlackStar/MyShop_BlackStar/ItemsRealm.swift
@@ -0,0 +1,14 @@
+import Foundation
+import RealmSwift
+
+class ItemsRealm: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var descript = ""
+    @objc dynamic var colorName = ""
+    @objc dynamic var mainImageURL = ""
+    dynamic var productImagesURL = List<String>()
+    @objc dynamic var size = ""
+    @objc dynamic var price = 0
+    @objc dynamic var oldPrice = 0
+    @objc dynamic var tag = ""
+}
