734e75e97682f2c712d4bb4569b30524663cc350
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2021-01-15 18:16:05 +0300

    Cells folder updated

---
+++ MyShop_BlackStar/MyShop_BlackStar/Cells/PictureCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PictureCollectionViewCell.swift
+//  MyShop_BlackStar
+//
+//  Created by Norik on 12.12.2020.
+//  Copyright © 2020 Norik. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
