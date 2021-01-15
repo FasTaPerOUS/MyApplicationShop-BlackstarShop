ba7652516845191b131f44c4760ae193ee191fc1
FasTaPerOUS <emailFasTaPerOUS@users.noreply.github.com>
2021-01-15 17:55:45 +0300

    Cells folder updated

---
+++ MyShop_BlackStar/MyShop_BlackStar/Cells/CategoryTableViewCell.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        logoImageView.layer.cornerRadius = 10 //logoImageView.bounds.height / 2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
